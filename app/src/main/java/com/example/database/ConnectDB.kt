b1668aac3e79739ba9a5fa9a553fb1aa4f259785
Young-Seok-Kim <x1682@naver.com>
2022-12-13 10:59:48 +0900

    프로젝트 생성

---
+++ app/src/main/java/com/example/database/ConnectDB.kt
@@ -0,0 +1,81 @@
+package com.example.database
+
+import android.content.Context
+import android.content.res.AssetManager
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.InputStream
+
+object ConnectDB {
+    // DB파일 불러오는 함수
+// assets 폴더에 db파일 넣어주면됨~
+
+    // DB파일 불러오는 함수
+    // assets 폴더에 db파일 넣어주면됨~
+    // /data/user/0/com.example.todayverse/databases/todayVerse.db
+    val ROOT_DIR = "/data/user/0/com.example.todayverse/databases/"
+    val DB_NAME = "todayVerse.db"
+
+    fun setDB(ctx: Context) {
+        val folder = File(ROOT_DIR)
+        if (folder.exists()) {
+        } else {
+            folder.mkdirs()
+        }
+        val assetManager: AssetManager = ctx.resources.assets
+        // db파일 이름 적어주기
+        val outfile = File("${ROOT_DIR}${DB_NAME}")
+        var `is`: InputStream? = null
+        var fo: FileOutputStream? = null
+        var filesize: Long = 0
+        try {
+            `is` = assetManager.open(DB_NAME, AssetManager.ACCESS_BUFFER)
+            filesize = `is`.available().toLong()
+            if (outfile.length() <= 0) {
+                val tempdata = ByteArray(filesize.toInt())
+                `is`.read(tempdata)
+                `is`.close()
+                outfile.createNewFile()
+                fo = FileOutputStream(outfile)
+                fo.write(tempdata)
+                fo.close()
+            }
+        } catch (e: IOException) {
+        }
+    }
+
+    // Cursor를 통해 .db파일의 내용을 보여주는 함수
+    var db: SQLiteDatabase? = null
+    lateinit var cursor: Cursor
+    var mHelper: ProductDBHelper? = null
+    var tableName = "android_metadata"
+
+    fun ShowMushDBInfo(mContext : Context, name: String) {
+        setDB(mContext)
+        mHelper = ProductDBHelper(mContext)
+        db = mHelper?.writableDatabase
+        var mushname: String? = null
+        var mushtype: String? = null
+        var mushsym: String? = null
+        var mushsim: String? = null
+        val sql = "Select * FROM ${tableName}"
+        cursor = db!!.rawQuery(sql, null)
+        while (cursor.moveToNext()) {
+            System.out.println("테스트 " + cursor.getString(0))
+//            if (cursor.getString(1).equals(name)) {
+//                mushname = cursor.getString(1)
+//                mushtype = cursor.getString(2)
+//                mushsym = cursor.getString(3)
+//                mushsim = cursor.getString(4)
+//            }
+        }
+//
+//        mname.setText(mushname)
+//        mtype.setText(mushtype)
+//        msym.setText(mushsym)
+//        msim.setText(mushsim)
+    }
+}
---
+++ app/src/main/java/com/example/database/ProductDBHelper.kt
@@ -0,0 +1,17 @@
+package com.example.database
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+//새로 생성한 adapter 속성은 SQLiteOpenHelper이다.
+
+class ProductDBHelper (context: Context?) : SQLiteOpenHelper(context, "todayVerse.db", null, 1) {
+    override fun onCreate(db: SQLiteDatabase) {
+        // TODO Auto-generated method stub
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase, oldVersion: Int, newVersion: Int) {
+        // TODO Auto-generated method stub
+    }
+}
